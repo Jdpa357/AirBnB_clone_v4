bb2eaf11f9d6130eb250a7120130327520484c9a
Luz <zulsb2093@gmail.com>
2020-05-17 03:31:58 +0000

    Add request.

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -26,4 +26,11 @@ $(document).ready(() => {
       $('div.amenities > h4').text(amenityString);
     }
   });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
 });
