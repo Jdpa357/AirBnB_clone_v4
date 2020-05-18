f30b79597157f7d3e0f6304de6979842bd6a1f09
Jdpa357 <jdpa352@hotmail.es>
2020-05-18 23:18:05 +0000

    Adding new AJAX function to make a POST request

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -24,3 +24,17 @@ $(document).ready(function () {
       $('#api_status').removeClass('available');
     }
   });
+
+ $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    type: 'POST',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let place = data[i];
+        $('.places ').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms + '</div><div class="number_bathrooms">' + place.number_bathrooms + '</div></div><div class="description">' + place.description + '</div></article>');
+      }
+    }
+  });
