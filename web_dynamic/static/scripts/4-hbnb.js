0ca95409323d516452d3f1d65f8c4387e1e12880
Jdpa357 <jdpa352@hotmail.es>
2020-05-18 23:16:02 +0000

    Adding new JS file to web dynamic folder

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,26 @@
+const $ = window.$;
+$(document).ready(function () {
+  const amens = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+        amens[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amens[$(this).data('id')];
+      }
+    });
+    if (Object.values(amens).length > 0) {
+      $('.amenities h4').text(Object.values(amens).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+    console.log(data);
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
