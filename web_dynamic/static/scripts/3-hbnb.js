81eb7be57000755c56705b53a578d8801a2bda6f
Jdpa357 <jdpa352@hotmail.es>
2020-05-18 23:07:59 +0000

    Adding get function to JS file

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -26,11 +26,27 @@ $(document).ready(() => {
       $('div.amenities > h4').text(amenityString);
     }
   });
+
   $.get('http://0.0.0.0:5001/api/v1/places_search/', function (data, status) {
     if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
       $('DIV#api_status').removeClass('available');
+    }
+  });
+});
+
+$.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    type: 'POST',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let place = data[i];
+        $('.places ').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms + '</div><div class="number_bathrooms">' + place.number_bathrooms + '</div></div><div class="description">' + place.description + '</div></article>');
+      }
     }
   });
 });
